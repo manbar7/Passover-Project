c782f74016b2b9c87399f3a1b0612035b2beb451
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-04-28 20:30:20 +0300

    Add files via upload

---
+++ ConsoleApp22/DAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class DAO
+    {
+        public void Insert(List<int> list, string table)
+        {
+                 using (SqlConnection conn = new SqlConnection(@"Data Source=.;Initial Catalog=Passover;Integrated Security=True"))
+                 {
+
+                foreach (int number in list)
+                {
+                    SqlCommand cmd = new SqlCommand($"INSERT INTO {table} VALUES {number}", conn);
+
+                    cmd.Connection.Open();
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Connection.Close(); 
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 1;
+            List<int> Xs = new List<int>();
+            List<int> Ys = new List<int>();
+            while (x != 0 && x > 0)
+            {
+                Console.WriteLine("Please enter a number for the X List");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x != 0 && x > 0)
+                {
+                    Xs.Add(x);
+                    Console.WriteLine("Please enter a number for the Y List");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    Ys.Add(y);
+
+
+                }
+            }
+
+            DAO dAO = new DAO();
+            dAO.Insert(Xs,"X");
+            dAO.Insert(Ys, "Y");
+
+
+
+        }
+    }
+}
